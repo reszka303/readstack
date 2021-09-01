99a9536c197a50759781b948c04373d0f34233bc
reszka303 <reszka303@op.pl>
2021-09-02 00:56:59 +0200

    10th commit: Password encryption

--- src/main/java/pl/javastart/readstack/domain/user/User.java
+++ src/main/java/pl/javastart/readstack/domain/user/User.java
@@ -39,4 +39,8 @@ public class User {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
